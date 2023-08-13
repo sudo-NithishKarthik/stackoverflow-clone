06f96bd44301f2ef73d05b512f40be91002141f0
Nithish <nithishkarthik01@gmail.com>
2023-08-13 14:56:39 +0530

    add support for subscription based features

--- client/src/pages/UserProfile/ProfileBio.js
+++ client/src/pages/UserProfile/ProfileBio.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const ProfileBio = ({ currentProfile }) => {
-	return (
-		<div className="current-user-profile-main">
-			<div className="user-tags">
-				{currentProfile?.tags.length !== 0 ? (
-					<>
-						<h4>Tags watched</h4>
-						{currentProfile?.tags.map((tag) => (
-							<p key={tag} className="user-all-tags">
-								{tag}
-							</p>
-						))}
-					</>
-				) : (
-					<p>0 tags watched</p>
-				)}
-			</div>
-			<div className="user-about">
-				{currentProfile?.about ? (
-					<>
-						<h4>About</h4>
-						<p className="user-about-para">{currentProfile?.about}</p>
-					</>
-				) : (
-					<p>No bio found</p>
-				)}
-			</div>
-		</div>
-	);
+  console.log({ currentProfile })
+  useEffect(() => {
+    const form = document.createElement("form")
+    const script = document.createElement('script')
+    script.id = 'razorpay'
+    script.src = 'https://checkout.razorpay.com/v1/payment-button.js'
+    script.async = true
+    script.setAttribute("data-payment_button_id", "pl_MPLF8esetNHPXU")
+    form.appendChild(script)
+    document.body.append(form)
+  }, [])
+  return (
+    <div className="current-user-profile-main">
+      <div className="user-tags">
+        {currentProfile?.tags.length !== 0 ? (
+          <>
+            <h4>Tags watched</h4>
+            {currentProfile?.tags.map((tag) => (
+              <p key={tag} className="user-all-tags">
+                {tag}
+              </p>
+            ))}
+          </>
+        ) : (
+          <p>0 tags watched</p>
+        )}
+      </div>
+      <div className="user-about">
+        {currentProfile?.about ? (
+          <>
+            <h4>About</h4>
+            <p className="user-about-para">{currentProfile?.about}</p>
+          </>
+        ) : (
+          <p>No bio found</p>
+        )}
+      </div>
+      <div>
+        Subscription Status: {currentProfile?.subscriptionStatus}
+      </div>
+      <form><script src="https://checkout.razorpay.com/v1/payment-button.js" async> </script> </form>
+    </div>
+  );
 };
 
 export default ProfileBio;
